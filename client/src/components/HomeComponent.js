d2f22ea3c2b8509eb97a1d10c7fc240fa5cebdec
RKRitik <khatri.ritik16@gmail.com>
2020-02-25 00:24:07 +0530

    fixed home

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -1,39 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { BOOKS } from '../shared/Books';
-import BooksCarousel from './CarouselComponent';
+import { BOOKS } from "../shared/Books";
+import BooksCarousel from "./CarouselComponent";
 
 const Home = () => {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+      <div className="row row-content">
+        <div className="col-12 col-sm-5 m-1">
+          <h2>Top Picks</h2>
+          <BooksCarousel books={BOOKS} />
+        </div>
+        <div className="col-12 col-sm-5 m-1 ">
+          <div className="col-12">
+            {" "}
+            <h2>Why choose us?</h2>
+          </div>
 
-        return (
-                <div className='container'>
-                        <h1>Home Page</h1>
-                        <div className='row row-content'>
-                                <div className='col-12 col-sm-5 m-1'>
-                                        <h2>Top Picks</h2>
-                                        <BooksCarousel books={BOOKS} />
-
-                                </div>
-                                <div className='col-12 col-sm-5 m-1 '>
-                                        <div className='col-12'> <h2>Why choose us?</h2></div>
-
-                                        <div className=' col-12  d-flex align-content-center'>
-                                                Maecenas a justo molestie, feugiat lacus a, suscipit purus. Duis nec varius lacus, eu tincidunt odio.
-                                                         Proin sollicitudin ipsum eu convallis imperdiet. Donec tempus rutrum metus, id pharetra lorem bibendum non.
-                                                         Cras sit amet orci sed leo condimentum rutrum vel non magna. Aliquam lobortis ipsum urna, eget laoreet metus
-                                                         gravida elementum. Nulla et venenatis lectus. Cras lacus massa, commodo non accumsan sit amet,
-                                                          eleifend quis risus. Donec eget mauris sit amet mauris congue tempus eget sit amet odio.
-                                       </div>
-
-
-
-
-                                </div>
-                        </div>
-                        {/* <RenderTopPicks book={someBook}/> */}
-
-                </div>
-        );
-}
+          <div className=" col-12  d-flex align-content-center">
+            What sets us apart from the competition is that we're passionate
+            about the products/services we provide and quality of support. We're
+            always looking to improve every aspect of our business. From more
+            features all the way to more frequent technologies.
+          </div>
+        </div>
+      </div>
+      {/* <RenderTopPicks book={someBook}/> */}
+    </div>
+  );
+};
 
 export default Home;
